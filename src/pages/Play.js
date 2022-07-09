a97556bc08c608801ed9d3aa0c68042dabbe28c4
yang <tiutni.dev@gmail.com>
2022-07-10 01:18:45 +0900

    원인 찾음, 리액트로 넘어오면서 video 태그의 autoplay 속성을 카멜케이스로 적어야함(소름돋는 건 그 어디에도 이 내용이 없음..)

--- src/pages/Play.js
+++ src/pages/Play.js
@@ -1,19 +1,16 @@
 import React from 'react'
 import '../css/Play.css'
 
-
-
 export default function Play() {
 
   return (
     <div>
       <video
-            id='vid'
             src={process.env.PUBLIC_URL + '/resources/이상한변호사.mp4'}
             poster={process.env.PUBLIC_URL + '/resources/이상한변호사.jpg'} 
-            autoplay={true}
-            muted={true}
-            controls={true}
+            autoPlay
+            muted
+            controls
         ></video>
     </div>
   );
