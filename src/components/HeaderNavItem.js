d9cf2464adc703cae92292b723c6520b46f2a5bc
yang <tiutni.dev@gmail.com>
2022-07-11 17:35:50 +0900

    data.map is not a function

--- src/components/HeaderNavItem.js
+++ src/components/HeaderNavItem.js
@@ -41,52 +41,7 @@ import '../css/Browse.css'
 // console.log(Object.keys(navItemId.navItemIds).length);
 // const navItemIdsLength = Object.keys(navItemId.navItemIds).length
 
-//
-
-const navItem = {
-    "words": [
-        "홈",
-        "시리즈",
-        "영화",
-        "NEW! 요즘 대세 콘텐츠",
-        "내가 찜한 콘텐츠"
-    ],
-    "ids" : [
-        "home",
-        "series",
-        "movie",
-        "trend",
-        "like"
-    ]
-}
-
-const MyNavItem = () => {
-
-    let result = [];
-
-    for(let i = 0; i < navItem.words.length; i++) {
-        result[i] = (
-            <div className="nav-item" id={navItem.words[i]}>
-                <a href='#'>{navItem.ids[i]}</a>
-            </div>
-        )
-    }
-
-    return (
-        result
-    )
-}
-
-export default function HeaderNavItem() {
-
-    return (
-        <Fragment>
-            {MyNavItem}
-        </Fragment>
-    )
-}
-
-//
+//-------------------------------------------------------------------
 
 // const navItemWord = {
 //     "navItemWords": [
@@ -114,3 +69,136 @@ export default function HeaderNavItem() {
 //         </Fragment>
 //     )
 // }
+
+//-------------------------------------------------------------------
+
+// const navItem = {
+//     "words": [
+//         "홈",
+//         "시리즈",
+//         "영화",
+//         "NEW! 요즘 대세 콘텐츠",
+//         "내가 찜한 콘텐츠"
+//     ],
+//     "ids" : [
+//         "home",
+//         "series",
+//         "movie",
+//         "trend",
+//         "like"
+//     ]
+// }
+
+// const MyNavItem = () => {
+
+//     let result = [];
+
+//     for(let i = 0; i < navItem.words.length; i++) {
+//         result[i] = (
+//             <div className="nav-item" id={navItem.words[i]}>
+//                 <a href='#'>{navItem.ids[i]}</a>
+//             </div>
+//         )
+//     }
+
+//     return (
+//         result
+//     )
+// }
+
+// export default function HeaderNavItem() {
+
+//     return (
+//         <Fragment>
+//             {MyNavItem}
+//         </Fragment>
+//     )
+// }
+
+//-------------------------------------------------------------------
+
+// function NavItemList (navItems) {
+//     return (
+//         const {words, ids} = navItems
+//         {navItems.map(navItem) => 
+//             return (
+//                 <HeaderNavItem
+//                     words={words}
+//                     ids={ids}
+//                 />
+//             );
+//         }
+//     )
+// }
+
+//
+
+// function MyNavItems () {
+
+//     const navItems = {
+//         "words": [
+//             "홈",
+//             "시리즈",
+//             "영화",
+//             "NEW! 요즘 대세 콘텐츠",
+//             "내가 찜한 콘텐츠"
+//         ],
+//         "ids" : [
+//             "home",
+//             "series",
+//             "movie",
+//             "trend",
+//             "like"
+//         ]
+//     }
+
+//     return (
+//         {navItems.map(navItems) => {
+//                 return (
+//                     <HeaderNavItem
+//                         words={words}
+//                         ids={ids}
+//                     />
+//                 );
+//             }
+//         }
+//     )
+// }
+
+//
+
+const navItems = {
+    "words": [
+        "홈",
+        "시리즈",
+        "영화",
+        "NEW! 요즘 대세 콘텐츠",
+        "내가 찜한 콘텐츠"
+    ],
+    "ids" : [
+        "home",
+        "series",
+        "movie",
+        "trend",
+        "like"
+    ]
+}
+
+const MyNavItems = (data) => data.map((Item) => {
+    const { words, ids } = Item;
+    return (
+        <div className="nav-item" id={ids}>
+            <a href='#'>{words}</a>
+        </div>
+    )
+})
+
+export default function HeaderNavItem() {
+
+    return (
+        <Fragment>
+            {MyNavItems(navItems)}
+        </Fragment>
+    )
+}
+
