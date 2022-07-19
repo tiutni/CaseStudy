c36d09d286619c37dcaff8f9d7f61d0f4c8989a4
yang <tiutni.dev@gmail.com>
2022-07-19 16:25:23 +0900

    로그인 기능완료

--- src/components/LoginBox.jsx
+++ src/components/LoginBox.jsx
@@ -1,13 +1,75 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import UserObj from '../data/UserObj';
 
 const LoginBox = () => {
+
+  const [email, setEmail] = useState('');
+    const [pw, setPw] = useState('');
+
+    const [emailValid, setEmailValid] = useState(false);
+    const [pwValid, setPwValid] = useState(false);
+    const [notAllow, setNotAllow] = useState(true);
+
+    useEffect(() => {
+      if(emailValid && pwValid) {
+        setNotAllow(false);
+        return;
+      }
+      setNotAllow(true);
+    }, [emailValid, pwValid]);
+
+    const handleEmail = (e) => {
+      setEmail(e.target.value);
+      const regex =
+        /^(([^<>()\[\].,;:\s@"]+(\.[^<>()\[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/i;
+      if (regex.test(e.target.value)) {
+        setEmailValid(true);
+      } else {
+        setEmailValid(false);
+      }
+    };
+    const handlePw = (e) => {
+      setPw(e.target.value);
+      const regex =
+        /^(?=.*[a-zA-z])(?=.*[0-9])(?=.*[$`~!@$!%*#^?&\\(\\)\-_=+])(?!.*[^a-zA-z0-9$`~!@$!%*#^?&\\(\\)\-_=+]).{8,20}$/;
+      if (regex.test(e.target.value)) {
+        setPwValid(true);
+      } else {
+        setPwValid(false);
+      }
+    };
+    const onClickConfirmButton = () => {
+      if(email === UserObj.users[0].email && pw === UserObj.users[0].pw) {
+        alert('로그인에 성공했습니다.')
+      } else {
+        alert("등록되지 않은 회원입니다.");
+      }
+    }
+
   return (
     <div>
       <div className="login-box">
         <h2>로그인</h2>
-        <input type="text" placeholder="이메일 주소 또는 전화번호" />
-        <input type="text" placeholder="비밀번호" />
-        <button>로그인</button>
+
+        <div className='inputWrap'>
+          <input className="input" type="text" placeholder="이메일 주소 또는 전화번호" value={email} onChange={handleEmail}/>
+        </div>
+        <div className='errorMessageWrap'>
+          {!emailValid && email.length > 0 && (
+              <div>정확한 이메일 주소를 입력하세요.</div>
+          )}
+        </div>
+        <div className='inputWrap'>
+          <input className="input" type="password" placeholder="비밀번호" value={pw} onChange={handlePw}/>
+        </div>
+        <div className='errorMessageWrap'>
+          {!pwValid && pw.length > 0 && (
+              <div>영문, 숫자, 특수문자 포함 4자 이상 입력해주세요.</div>
+          )}
+        </div>
+        <button disabled={notAllow} className='bottomButton' onClick={onClickConfirmButton}>로그인</button>
+
         <div className="login-guid-text">
           <div>
             <input type="checkbox" defaultChecked />
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,4 +1,3 @@
-import { Link } from "react-router-dom";
 import Footer from "../components/Footer";
 import LoginBox from "../components/LoginBox";
 import LoginLogo from "../components/LoginLogo";
